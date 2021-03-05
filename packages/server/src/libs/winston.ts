cccc8e507093a179fc3216218f65eeffd19bd7d9
Mai Tung <goingdownfr@gmail.com>
2021-03-05 21:34:43 +0700

    [add] setup logs + helmet

---
+++ packages/server/src/libs/winston.ts
@@ -0,0 +1,36 @@
+import appRoot from 'app-root-path';
+import winston from 'winston';
+
+const options = {
+  file: {
+    level: 'info',
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
+// instantiate a new Winston Logger with the settings defined above
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console),
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
+
+export class LoggerStream {
+  // eslint-disable-next-line class-methods-use-this
+  write(message: string) {
+    logger.info(message.replace('"', ' ').substring(0, message.lastIndexOf('\n')));
+  }
+}
