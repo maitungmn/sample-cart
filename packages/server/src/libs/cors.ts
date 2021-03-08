81fd3d483207dd2116bac4b0eaf118a2e0293e19
Mai Tung <goingdownfr@gmail.com>
2021-03-08 11:57:43 +0700

    update cors

--- packages/server/src/libs/cors.ts
+++ packages/server/src/libs/cors.ts
@@ -1,5 +1,5 @@
 export default () => {
-  const allowedOrigins = ['http://206.189.40.114:3050/', 'http://localhost:3000'];
+  const allowedOrigins = ['http://206.189.40.114:3050', 'http://10.104.0.2:3050', 'http://localhost:3000'];
   return {
     origin(origin: string, callback: (arg0: Error | null, arg1: boolean) => any) {
       // allow requests with no origin
